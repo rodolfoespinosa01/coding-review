83c6586dfe6ce229312f31704c0941e8014e42de
Rodolfo <rodolfoespinosa01@gmail.com>
2023-11-25 02:32:34 -0500

    rockspaperscissor complete

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // Tic Tac Toe done in node
+const userChoice = prompt("Enter your choice");
 const rocks = "rocks";
 const paper = "papers";
 const scissors = "scissors";
@@ -10,3 +11,17 @@ const randomNumber = Math.floor(Math.random() * 3);
 computersChoice = weapons[randomNumber];
 
 console.log(computersChoice);
+
+console.log(userChoice);
+
+if (userChoice === computersChoice) {
+  console.log("It's a tie!");
+} else if (
+  (userChoice === rocks && computersChoice === scissors) ||
+  (userChoice === paper && computersChoice === rocks) ||
+  (userChoice === scissors && computersChoice === paper)
+) {
+  console.log("You win!");
+} else {
+  console.log("Computer wins!");
+}
