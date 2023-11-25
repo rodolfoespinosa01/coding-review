b017f3f8d82b4eebdab2710558b4b34b1fc3cf5b
Rodolfo <rodolfoespinosa01@gmail.com>
2023-11-25 01:41:04 -0500

    answer

--- server.js
+++ server.js
@@ -2,17 +2,8 @@
 // return true if one of the number is 100 or if the sum of the two numbers is 100
 
 function checkAndReturn(num1, num2) {
-  if (num1 === 100) {
-    console.log("One of the numbers equals 100");
-  }
-
-  if (num2 === 100) {
-    console.log("One of the numbers equals 100");
-  }
-
-  if (num1 + num2 === 100) {
-    console.log("The sum of the two numbers equals 100");
-  }
+  return num1 === 100 || num2 === 100 || num1 + num2 === 100;
 }
+let answer = checkAndReturn(100, 42);
 
-checkAndReturn(100, 25);
+console.log(answer);
