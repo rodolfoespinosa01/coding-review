01835fdc959affb421b3db42aa75ab432885aea3
Rodolfo <rodolfoespinosa01@gmail.com>
2023-11-26 21:07:07 -0500

    Server side app complete for authentication

--- server/server.js
+++ server/server.js
@@ -1,15 +1,22 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 
 const app = express();
-
 const PORT = process.env.PORT || 3333;
+
+require("dotenv").config();
 
 const user_routes = require("./routes/user_routes");
 
 const db = require("./config/connection");
 
+// Open channel for JSON to be sent from client
 app.use(express.json());
 
+// Open cookie middleware channel so we can view cookies on the request object
+app.use(cookieParser());
+
+// Load Routes
 app.use("/auth", user_routes);
 
 // Validate that the mongoose connection is complete
